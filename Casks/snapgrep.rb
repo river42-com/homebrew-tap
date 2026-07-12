8094b56ed94daa10240554983a5d075a0a328fb1
Wolfgang Kerschbaumer <wolfgang.kerschbaumer@sysinit.at>
2026-07-12 17:43:18 +0200

    snapgrep 0.27.2

--- Casks/snapgrep.rb
+++ Casks/snapgrep.rb
@@ -1,6 +1,6 @@
 cask "snapgrep" do
-  version "0.27.1,74"
-  sha256 "d10b9523b94b56f16e869b955650598913ad9349220073c7b51bad4c1c777c59"
+  version "0.27.2,80"
+  sha256 "c6a26d135fa4e403f5311460482ac4248f38bcbfd1685f9ff9e6391cd927d1a2"
 
   url "https://snapgrep.com/updates/SnapGrep-#{version.csv.first}.zip"
   name "SnapGrep"
